e8544f73bda717a8bd150d0038255f9217038723
mehmet787 <mehmetermss10@gmail.com>
2024-10-11 14:08:05 +0300

    Updated grafik.py with new content

---
+++ grafik.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+
+# Excel dosyasını oku
+file_path = r'C:\Users\it.stajyer\Desktop\Dijital_Olgunluk.xlsx'
+df_strateji = pd.read_excel(file_path, sheet_name='1-Strateji')
+
+# X sayımlarını hesapla
+KKM_count = 0
+KM_count = 0
+K_count = 0
+KK_count = 0
+
+for i in range(3, 13):
+    if df_strateji.loc[i, 'KKM'] == 'X':
+        KKM_count += 1
+    if df_strateji.loc[i, 'KM'] == 'X':
+        KM_count += 1
+    if df_strateji.loc[i, 'K'] == 'X':
+        K_count += 1
+    if df_strateji.loc[i, 'KK'] == 'X':
+        KK_count += 1
+
+# Sonuçları DataFrame'e yaz
+df_strateji.loc[13, 'KKM'] = KKM_count
+df_strateji.loc[13, 'KM'] = KM_count
+df_strateji.loc[13, 'K'] = K_count
+df_strateji.loc[13, 'KK'] = KK_count
+
+# Streamlit arayüzü
+st.title("X Değerleri Sayımı")
+
+# Sonuçları göster
+st.write("KKM sütunundaki X sayısı:", KKM_count)
+st.write("KM sütunundaki X sayısı:", KM_count)
+st.write("K sütunundaki X sayısı:", K_count)
+st.write("KK sütunundaki X sayısı:", KK_count)
+
+# Grafik oluştur
+labels = ['KKM', 'KM', 'K', 'KK']
+counts = [KKM_count, KM_count, K_count, KK_count]
+
+fig, ax = plt.subplots()
+ax.bar(labels, counts, color=['blue', 'orange', 'green', 'red'])
+ax.set_ylabel('X Sayısı')
+ax.set_title('Sütunlardaki X Değerlerinin Sayısı')
+
+# Grafiği Streamlit'te göster
+st.pyplot(fig)
+
+
